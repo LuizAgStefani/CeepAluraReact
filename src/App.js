74b535d53c6a49bc31e9d8876272c587170f6b0d
Luiz Augusto Stefani <luiz.luiz501@gmail.com>
2021-07-26 18:37:07 -0300

    Adicionado todos os arquivos ao Git

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import { Component } from 'react';
+import './App.css';
+import FormularioCadastro from './components/FormularioCadastro';
+import ListaDeNotas from "./components/ListaDeNotas"
+
+class App extends Component {
+  render() {
+    return (
+      <section>
+        <FormularioCadastro />
+        <ListaDeNotas />
+      </section>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/ListaDeNotas.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import CardNota from "./CardNota/CardNota";
+
+class ListaDeNotas extends Component {
+
+    render() {
+        return (
+            <ul>
+                {Array.of("Trabalho", "Trabalho", "Estudos", "Jogos").map((categorias, index) => {
+                    return (
+                        <li key={index}>
+                            <CardNota />
+                        </li>
+                    );
+                })
+
+                }
+
+            </ul>
+        );
+    }
+}
+
+export default ListaDeNotas;

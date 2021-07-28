b74ea3164bef9566b11be19babe2a738c8af79b5
Luiz Augusto Stefani <luiz.luiz501@gmail.com>
2021-07-28 19:21:26 -0300

    Finalizado 1/3 ultima aula

--- src/components/FormularioCadastro/FormularioCadastro.jsx
+++ src/components/FormularioCadastro/FormularioCadastro.jsx
@@ -3,29 +3,45 @@ import "./estilo.css"
 
 class FormularioCadastro extends Component {
 
-    constructor(){
-        super();
+    // Para usar propriedades de fora dessa classe é preciso passar o props no constructor
+    constructor(props){
+        super(props);
         this.titulo = "";
+        this.texto = "";
     }
 
-    handleMudancaTitulo(evento){
+    _handleMudancaTitulo(evento){
         this.titulo = evento.target.value;
-        console.log(this.titulo);
+        evento.stopPropagation();
+    }
+
+    _handleMudancaTexto(evento){
+        this.texto = evento.target.value;
+        evento.stopPropagation();
+    }
+    _criarNota(evento){
+        evento.preventDefault();
+        evento.stopPropagation();
+        this.props.criarNota(this.titulo, this.texto);
     }
 
     render() {
         return (
-            <form className="form-cadastro">
+            <form 
+            className="form-cadastro"
+            onSubmit={this._criarNota.bind(this)}
+            >
                 <input
                     type="text"
                     placeholder="Título"
                     className="form-cadastro_input"
-                    onChange={this.handleMudancaTitulo.bind(this)}//é necessário esse bind na hora de criar um evento
+                    onChange={this._handleMudancaTitulo.bind(this)}//é necessário esse bind na hora de criar um evento
                 />
                 <textarea
                     rows={15}
                     placeholder="Escreva sua nota..."
                     className="form-cadastro_input"
+                    onChange={this._handleMudancaTexto.bind(this)}
                 />
                 <button className="form-cadastro_input form-cadastro_submit">
                     Criar Nota
